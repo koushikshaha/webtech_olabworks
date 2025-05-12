57ab47ee60665911e6b8728e7b08e7a3b40706ca
KOUSHIK SAHA <koushiksaahaa@outlook.com>
2025-05-12 09:53:08 +0600

    first php

---
+++ index.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP SUCCESS</title>
+</head>
+<body>
+       <h1>PHP INPUT</h1>
+       <form action="external.php" method="post">
+        <div>
+        <input type="text" name="fullname" placeholder="Name">
+        </div>
+        
+        <div>
+        <input type="password" name = "password" placeholder = "password">
+        </div>
+
+        <div>
+        <input type="password" name = "password" placeholder = "password">
+        </div>
+
+        <div>
+        <input type="email" name="email" placeholder="Email">
+        </div>
+
+        <div>
+        <input type="submit" name="submit" value="submit"> 
+        </div>
+
+        
+        
+        
+
+        
+
+
+
+       </form>   
+</body>
+</html>
